7abfb7770c9e899efd59c0d39da284f0a6c98dc0
grig <prowlergrig@yahoo.ie>
2023-05-20 11:55:19 +0300

    ignore errors in print script

--- saves/printnewsave.py
+++ saves/printnewsave.py
@@ -91,42 +91,47 @@ while True:
         # Check if file has the correct extension
         if os.path.splitext(filename)[1] == file_extension:
 
-            # Get the default printer name
-            printer_name = win32print.GetDefaultPrinter()
-            hDC = win32ui.CreateDC()
-            hDC.CreatePrinterDC(printer_name)
-            printable_area = hDC.GetDeviceCaps(HORZRES), hDC.GetDeviceCaps(VERTRES)
-            printer_size = hDC.GetDeviceCaps(PHYSICALWIDTH), hDC.GetDeviceCaps(PHYSICALHEIGHT)
-            printer_margins = hDC.GetDeviceCaps(PHYSICALOFFSETX), hDC.GetDeviceCaps(PHYSICALOFFSETY)
+            try:
+                # Get the default printer name
+                printer_name = win32print.GetDefaultPrinter()
+                hDC = win32ui.CreateDC()
+                hDC.CreatePrinterDC(printer_name)
+                printable_area = hDC.GetDeviceCaps(HORZRES), hDC.GetDeviceCaps(VERTRES)
+                printer_size = hDC.GetDeviceCaps(PHYSICALWIDTH), hDC.GetDeviceCaps(PHYSICALHEIGHT)
+                printer_margins = hDC.GetDeviceCaps(PHYSICALOFFSETX), hDC.GetDeviceCaps(PHYSICALOFFSETY)
 
-            # Print the file
-            file_path = os.path.join(folder_to_monitor, filename)
-            print(f"Printing {filename}...")
+                # Print the file
+                file_path = os.path.join(folder_to_monitor, filename)
+                print(f"Printing {filename}...")
+                time.sleep(0.5)
 
-            bmp = Image.open(file_path)
-            if bmp.size[0] > bmp.size[1]:
-                bmp = bmp.rotate(90)
+                bmp = Image.open(file_path)
+                if bmp.size[0] > bmp.size[1]:
+                    bmp = bmp.rotate(90)
 
-            ratios = [1.0 * printable_area[0] / bmp.size[0], 1.0 * printable_area[1] / bmp.size[1]]
-            scale = min(ratios)
+                ratios = [1.0 * printable_area[0] / bmp.size[0], 1.0 * printable_area[1] / bmp.size[1]]
+                scale = min(ratios)
 
-            hDC.StartDoc(file_path)
-            hDC.StartPage()
+                hDC.StartDoc(file_path)
+                hDC.StartPage()
 
-            dib = ImageWin.Dib(bmp)
-            scaled_width, scaled_height = [int(scale * i) for i in bmp.size]
+                dib = ImageWin.Dib(bmp)
+                scaled_width, scaled_height = [int(scale * i) for i in bmp.size]
 
-            x1 = int((printer_size[0] - scaled_width) / 2)
-            y1 = 0 # int((printer_size[1] - scaled_height) / 2)
-            x2 = x1 + scaled_width
-            y2 = y1 + scaled_height
-            dib.draw(hDC.GetHandleOutput(), (x1, y1, x2, y2))
+                x1 = int((printer_size[0] - scaled_width) / 2)
+                y1 = 0 # int((printer_size[1] - scaled_height) / 2)
+                x2 = x1 + scaled_width
+                y2 = y1 + scaled_height
+                dib.draw(hDC.GetHandleOutput(), (x1, y1, x2, y2))
 
-            hDC.EndPage()
-            hDC.EndDoc()
-            hDC.DeleteDC()
-                    
-            print(f"{filename} has been printed.")
+                hDC.EndPage()
+                hDC.EndDoc()
+                hDC.DeleteDC()
+                        
+                print(f"{filename} has been printed.")
+
+            except Exception: 
+                pass
 
     # Update processed_files set
     processed_files = files
